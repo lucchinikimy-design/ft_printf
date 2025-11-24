079c6d69911015fcf82ff688ab5810ad446b671a
klucchin <lucchini.kimy@gmail.com>
2025-11-24 16:27:33 +0100

    hex printing function for consistency

--- ft_print_hex.c
+++ ft_print_hex.c
@@ -12,21 +12,24 @@
 
 #include "ft_printf.h"
 
-static int find_hex(unsigned int n, const char *digits)
+static int	find_hex(unsigned int n, const char *digits)
 {
-    int count;
+	int		count;
+	char	c;
 
-    count = 0;
-    if (n >= 16)
-        count += find_hex(n / 16, digits);
-    char c = digits[n % 16];
-    count += write(1, &c, 1);
-    return (count);
+	count = 0;
+	if (n >= 16)
+		count += find_hex(n / 16, digits);
+	c = digits[n % 16];
+	count += write(1, &c, 1);
+	return (count);
 }
 
-int ft_print_hex(unsigned int n, const char spec)
+int	ft_print_hex(unsigned int n, char spec)
 {
-    if (spec == 'x')
-        return (find_hex(n, "0123456789abcdef"));
-    return (find_hex(n, "0123456789ABCDEF"));
+	if (spec == 'x')
+		return (find_hex(n, "0123456789abcdef"));
+	if (spec == 'X')
+		return (find_hex(n, "0123456789ABCDEF"));
+	return (0);
 }
